e175041965ba72a845f8b96794e0dc35c30ad289
r.odokienko <r.odokiienko@upstars.com>
2024-11-19 18:47:36 +0200

    added prometheus

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 sr_config = {
-    'url': 'http://schemaregistry:8085'
+    'url': 'http://schema-registry:8085'
 }
 config = {
     'bootstrap.servers': "kafka-broker-1:9092, kafka-broker-2:9092, kafka-broker-3:9092"
